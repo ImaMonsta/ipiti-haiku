4f83b47edfcff982f5a08e20fb0354edfcb4ffca
German <me@g3rm4n.xyz>
2018-08-25 00:17:00 -0500

    📦: Sentry releases

---
+++ scripts/sentry.sh
@@ -0,0 +1,16 @@
+# Create a new release
+echo "$1"
+curl https://sentry.io/api/0/organizations/g3rm4n/releases/ \
+  -X POST \
+  -H "Authorization: Bearer $1" \
+  -H 'Content-Type: application/json' \
+  -d "
+{
+    \"version\": \"$2\",
+    \"refs\": [{
+        \"repository\":\"$3\",
+        \"commit\":\"$2\"
+    }],
+    \"projects\":[\"$4\"]
+}
+"
