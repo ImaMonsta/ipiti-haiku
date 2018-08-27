1c7ef635332a942a8b07fea8a1b84ec9ec3e4db0
German <me@g3rm4n.xyz>
2018-08-27 10:46:24 -0500

    🐛: Fix test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Main from './views/Main';
+import Home from './views/Home';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Main />, div);
+  ReactDOM.render(<Home />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
