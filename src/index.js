08ad420b201d96bd3559c1b9818bc2dd8231ed06
Giovanny Rodriguez <giovanny@Giovannys-MacBook-Pro.local>
2018-09-17 20:24:50 -0500

    i18n wrapper moved out from index.js and added to Root.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { I18nextProvider } from 'react-i18next';
 import Raven from 'raven-js';
 import firebase from 'firebase/app';
 import Root from './config/Root';
-import i18next from './components/common/i18n';
 
 Raven.config('https://8ab951e8d78f4b3d8de1ca4b641b50fb@sentry.io/1265167', {
   // 👾
@@ -21,9 +19,4 @@ const config = {
 
 firebase.initializeApp(config);
 
-ReactDOM.render(
-  <I18nextProvider i18n={i18next}>
-    <Root />
-  </I18nextProvider>,
-  document.getElementById('root')
-);
+ReactDOM.render(<Root />, document.getElementById('root'));
